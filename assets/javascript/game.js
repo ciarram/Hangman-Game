568165c6c56c2a76189d7759b9c82f1317e19cf6
Ciarra Murphy <mciarra@ymail.com>
2017-08-23 23:04:38 -0400

    updated html and attempting js if else

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,25 @@
-var authors = ["austen", "dickens", "brone", "twain", "hemingway", "faulkner", "hurston"];
+var authors = ["austen", "dickens", "bronte", "twain", "hemingway", "faulkner", "hurston"];
 var numberOfGuess = 12;
 var win = 0;
 
 var authorName = authors[Math.floor(Math.random() * authors.length)];
     console.log("I've chosen one!");
+    console.log('authorName = %s',authorName);
+var authorLetters = str.indexof(authorName);
+    console.log("I'm an individual letter");
 
 document.onkeyup = function(event) {
     var playerGuess = event.key;
     console.log('The game starts');
-}
 
-
-if(playerGuess == authorName[]){
-    console.log("I got a letter");
-} else{
-    console.log("I lost a guess");
-    numberOfGuess--;
+        if(playerGuess == authorName.indexOf()){
+            console.log("I got a letter");
+        } else if (playerGuess != authorName){
+            console.log("I lost a guess");
+            numberOfGuess--;
+        } else if(numberOfGuess == 0){
+            console.log("start over");
+            authors[Math.floor(Math.random() * authors.length)];
+            numberOfGuess = 12;
+        }
 }
